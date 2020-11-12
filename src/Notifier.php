0b409ea99edf3fbd2772a7267929333655028608
e-welina <>
2020-11-12 13:50:40 +0100

    Update Nofifier

--- src/Notifier.php
+++ src/Notifier.php
@@ -50,5 +50,51 @@ class Notifier extends AirbrakeNotifier
 
         return $notice;
     }
+    
+    protected function gitRevision($dir)
+    {
+        $headFile = join(DIRECTORY_SEPARATOR, [$dir, '.git', 'HEAD']);
+        return null;
+        $head = @file_get_contents($headFile);
+        if ($head === false) {
+            return null;
+        }
+
+        $head = rtrim($head);
+        $prefix = 'ref: ';
+        if (strpos($head, $prefix) === false) {
+            return $head;
+        }
+        $head = substr($head, strlen($prefix));
+
+        $refFile = join(DIRECTORY_SEPARATOR, [$dir, '.git', $head]);
+        $rev = @file_get_contents($refFile);
+        if ($rev !== false) {
+            return rtrim($rev);
+        }
+
+        $refsFiles = join(DIRECTORY_SEPARATOR, [$dir, '.git', 'packed-refs']);
+        $handle = fopen($refsFiles, 'r');
+        if (!$handle) {
+            return null;
+        }
+
+        while (($line = fgets($handle)) !== false) {
+            if (!$line || $line[0] === '#' || $line[0] === '^') {
+                continue;
+            }
+
+            $parts = explode(' ', rtrim($line));
+            if (count($parts) !== 2) {
+                continue;
+            }
+
+            if ($parts[1] == $head) {
+                return $parts[0];
+            }
+        }
+
+        return null;
+    }
 
 }
