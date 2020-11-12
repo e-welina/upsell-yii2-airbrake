a556fdef59b54f140268c6f10bb833bcc991246d
e-welina <>
2020-11-12 12:56:49 +0100

    Notifier & AirbrakeService

---
+++ src/Notifier.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace upsell\yii2\airbrake;
+
+use Airbrake\Notifier as AirbrakeNotifier;
+use GuzzleHttp\Client;
+use Yii;
+
+class Notifier extends AirbrakeNotifier
+{
+    private $httpClient;
+   
+    public function __construct($opt)
+    {
+        parent::__construct($opt);
+        $this->httpClient = $this->newHTTPClient();
+        
+    }
+    
+    public function getClient(){
+        return $this->httpClient;
+    }
+
+    private function newHTTPClient()
+    {
+        return new Client([
+            'connect_timeout' => 5,
+            'read_timeout' => 5,
+            'timeout' => 5,
+            'verify' => false
+        ]);
+       
+    }
+    
+    
+    public function buildNotice($exc)
+    {
+        $notice = parent::buildNotice($exc);
+
+        if (isset(Yii::$app->user)) {
+            $user = Yii::$app->user;
+            if (isset($user->id)) {
+                $notice['context']['user']['id'] = $user->id;
+            }
+            if (isset($user->identity)) {
+                $notice['context']['user']['name'] = $user->identity->nombre;
+                $notice['context']['user']['email'] = $user->identity->email;
+            }
+        }
+
+        return $notice;
+    }
+
+}
