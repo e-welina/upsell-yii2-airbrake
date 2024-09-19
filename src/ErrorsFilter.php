b9b1892faada19a2eeae6f3f321d05480b14737e
EGA <>
2024-09-19 14:01:58 +0200

    Add error filter

---
+++ src/ErrorsFilter.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace upsell\airbrake;
+
+use Closure;
+use yii\base\BaseObject;
+
+class ErrorsFilter extends BaseObject
+{
+
+    /**
+     * Array of errors to ignore
+     * @var array
+     */
+    public $errors = [];
+
+
+   
+    private $errorsFilter;
+
+    public function init()
+    {
+        parent::init();
+
+        $this->errorsFilter = function ($notice) {
+
+                if ( in_array($notice['errors'][0]['type'],  $this->errors)) {
+                 return null;
+                }
+
+            return $notice;
+        };
+    }
+
+    /**
+     * Returns a callable that replaces occurrences of $params values
+     * with value specified in $replacement.
+     * @return callable|Closure Closure to set as filter
+     */
+    public function getErrorsFilter()
+    {
+        return $this->errorsFilter;
+    }
+
+}
